6355e172b08a27b05377560946645ed5167bd764
Zaid Dweiri <zaiddweiri962@gmail.com>
2025-10-19 13:20:33 +0300

    add the UI

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import stc from "string-to-color";
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export const stringToColor = (str: string) => {
-  return stc(str);
-};
